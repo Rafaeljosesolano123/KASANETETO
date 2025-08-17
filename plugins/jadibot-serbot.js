2c62ff922ec5f91df542283aea4a71ae3fdefa2f
pukarumi23 <edwinsantacruzh@gmail.com>
2025-08-17 15:31:16 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -1,3 +1,5 @@
+//🔶 no debe funcionar 
+/*
 const { useMultiFileAuthState, DisconnectReason, makeCacheableSignalKeyStore, fetchLatestBaileysVersion, Browsers } = (await import("@whiskeysockets/baileys"));
 import qrcode from "qrcode"
 import NodeCache from "node-cache"
@@ -367,3 +369,4 @@ async function joinChannels(conn) {
         await conn.newsletterFollow(channelId).catch(() => {})
     }
 }
+*/
