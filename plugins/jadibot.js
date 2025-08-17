94bd2eeb63064eebcdc3596126f528b4dc7b2adb
pukarumi23 <edwinsantacruzh@gmail.com>
2025-08-17 14:36:06 -0500

    🔶INDEPENDIENTE_BOT🔶

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -1,3 +1,4 @@
+/*
 import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as fsPromises } from "fs";
 const fs = { ...fsPromises, existsSync };
 import path, { join } from 'path' 
@@ -80,3 +81,4 @@ handler.help = ['sockets', 'deletesesion', 'pausarai']
 handler.command = ['deletesesion', 'deletebot', 'deletesession', 'deletesession', 'stop', 'pausarai', 'pausarbot', 'bots', 'sockets', 'socket']
 
 export default handler
+*/
